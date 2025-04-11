2f3242534fe84fc098652909621e3a83051d7b47
mbn-code <66748817+mbn-code@users.noreply.github.com>
2025-04-11 13:43:27 +0200

    more assets and fix errors

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,8 +1,9 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.CrossPlatformInput;
 
-[RequireComponent(typeof (GUITexture))]
+[RequireComponent(typeof (Image))]
 public class ForcedReset : MonoBehaviour
 {
     private void Update()
@@ -11,7 +12,7 @@ public class ForcedReset : MonoBehaviour
         if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
         {
             //... reload the scene
-            Application.LoadLevelAsync(Application.loadedLevelName);
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
         }
     }
 }
